e9e36e21df4ac73993e05a9a96f8bd97b8fdb75d
Hua Wang <oopsfoo@gmail.com>
2019-11-12 02:18:30 -0800

    add file links script

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-now=$(date +"%Y-%m-%d_%H_%M_%S")
-workpath="/tmp/clashup_$now/"
-mkdir -p $workpath
-pushd $workpath
+now_ts=$(date +"%Y-%m-%d_%H_%M_%S")
+work_path="/tmp/clashup_$now_ts/"
+curr_path=$(dirname "$(readlink -f "$0")")
+root_path=$(dirname $curr_path)
+
+mkdir -p $work_path
+pushd $work_path
 curl -s https://api.github.com/repos/Dreamacro/clash/releases\
 	| grep 'browser_download_url.*linux-amd64'\
 	| tail -n1\
@@ -13,10 +16,16 @@ tarball=$(ls)
 echo $tarball
 gunzip -c $tarball > clash
 chmod +x clash
-mv ./clash /opt/clash/bin
-rm -fr $workpath
+mv ./clash $curr_path
+popd
+rm -fr $work_path
+
+sudo ln -sf $root_path /opt/clashup
+
 #TODO get configuration from remote
+
+sed -i "s/oopsfoo/$USER/g" ../conf/supervisor.conf
+sudo ln -sf "$(readlink -f ../conf/supervisor.conf)" /etc/supervisor/conf.d/clashup.conf
 sudo apt install -y supervisor
-sudo ln -s /opt/clash/conf/supervisor.conf /etc/supervisor/conf.d/clash.conf
 sudo service supervisor restart
 sudo supervisorctl status
